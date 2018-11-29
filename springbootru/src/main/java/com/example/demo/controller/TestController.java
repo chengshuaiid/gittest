001921e96db7e8226e0a0b1ab305652df9b58ec4
chengshuai <1310327272@qq.com>
2018-11-29 14:28:21 +0800

    test

---
+++ springbootru/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by hzl-coding on 2018/11/29.
+ */
+@RestController
+public class TestController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/template/{id}")
+    public User findById(@PathVariable Long id) {
+        User u = this.restTemplate.getForObject("http://localhost:8091/user/" + id,User.class);
+        //System.out.println(u);
+        return u;
+    }
+}
