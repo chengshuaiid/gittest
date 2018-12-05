422daac40517ded97d5cb9a4d3166947c40d1dff
chengshuai <1310327272@qq.com>
2018-12-05 17:38:08 +0800

    test33

---
+++ springbootau/src/main/java/com/example/demo/test/gittest.java
@@ -0,0 +1,10 @@
+package com.example.demo.test;
+
+/**
+ * Created by hzl-coding on 2018/12/5.
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("tst");
+    }
+}
